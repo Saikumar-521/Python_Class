1c20a02dd365f0101010e4a9d39c4934152c8826
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-10-15 17:41:28 +0530

    module

---
+++ Day_15_module/main.py
@@ -0,0 +1,19 @@
+'''
+A module in Python is a file that contains Python code, such as functions, classes, or variables,
+which can be imported and reused in other Python programs.
+
+Types of Modules:
+
+Built-in Modules:
+These come pre-installed with Python.
+Example: math, os, datetime
+
+User-defined Modules:
+These are modules you create yourself.
+
+External Modules:
+Installed using package managers like pip.
+Example: numpy, pandas, requests
+'''
+import userdefined as ud
+
