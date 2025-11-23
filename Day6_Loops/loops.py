e3fbe398a3266a8f66c4ad815ad0936150e7e184
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-11-23 15:06:54 +0530

    Loops

--- Day6_Loops/loops.py
+++ Day6_Loops/loops.py
@@ -183,3 +183,13 @@ for i in range(1, 6):
 x = 10
 assert x > 0
 print("x is positive")
+
+n=100
+for i in range(1,n+1):
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c=c+1
+    if c==2:
+        print(i,'is a prime number')
+
